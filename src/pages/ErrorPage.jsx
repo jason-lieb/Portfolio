ecc17afde54c3b032795417c0a9509708de4fb9f
Jason Lieb <Jason.lieb@outlook.com>
2023-03-01 13:54:33 -0500

    create error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,3 +1,12 @@
+import { Container } from 'react-bootstrap'
+
 export default function ErrorPage() {
-  return <div></div>
+  return (
+    <Container style={{ padding: '40vh 0' }}>
+      <h3 className="text-center">This page does not exist</h3>
+      <h4 className="text-center">
+        Please choose a page from the navigation bar
+      </h4>
+    </Container>
+  )
 }
